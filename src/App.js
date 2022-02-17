cd9aaf93b64458954cef17b2c7ab743b28389e95
Gabriel S. Souza <gabriel12saraiva@gmail.com>
2022-02-17 14:20:05 -0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+const Titulo = ({ text, ...props }) => {
+	return <h1 {...props}>{text}</h1>;
+};
+
 const App = () => {
-	return <div>Meu App</div>;
+	return (
+		<div>
+			<Titulo id="2" style={{ color: "blue" }} text={"olá"} />
+			<Titulo id="2" style={{ fontSize: "100px" }} text={"mundo"} />
+		</div>
+	);
 };
 
 export default App;
